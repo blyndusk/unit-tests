06a8d86ff1fa4af10606ae21fcdd5bb13b9e2c55
blyndusk <alexandredelaloydev@gmail.com>
2019-04-10 15:46:14 +0200

    ✅ fix tests

--- test/fizzbuzz.specs.js
+++ test/fizzbuzz.specs.js
@@ -7,8 +7,8 @@ describe("Test of the test framework", () => {
 describe("Fizzbuzz sould return number", () => {
     it("init test", () => expect(fizzbuzz(1)).equal(1));
     it(" 15 => FIZZBUZZ", () => expect(fizzbuzz(15)).equal("FIZZBUZZ"));
-    it(" 5 => BUZZ", () => expect(fizzbuzz(15)).equal("BUZZ"));
-    it(" 3 => FIZZ", () => expect(fizzbuzz(3)).equal("FIZZBUZZ"));
+    it(" 5 => BUZZ", () => expect(fizzbuzz(5)).equal("BUZZ"));
+    it(" 3 => FIZZ", () => expect(fizzbuzz(3)).equal("FIZZ"));
     it(" 12 => FIZZ", () => expect(fizzbuzz(12)).equal("FIZZ"));
     it(" 10 => BUZZ", () => expect(fizzbuzz(10)).equal("BUZZ"));
 });
